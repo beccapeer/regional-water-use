8b9e0e17d7fe4a3389dc27e49d5f9cfc020fd0e9
Rebecca Peer <Becca@Rebeccas-MacBook-Pro.local>
2018-01-17 18:40:23 -0800

    combining EIA fuel codes

--- EIA-data-processing.R
+++ EIA-data-processing.R
@@ -13,7 +13,7 @@ wd            = '~/Documents/Graduate/Journal Papers/in-progress/EST_regional-wa
 eia860.folder = '~/Documents/Graduate/Journal Papers/in-progress/EST_regional-water-rates/data/2014 EIA 860/'
 eia923.folder = '~/Documents/Graduate/Journal Papers/in-progress/EST_regional-water-rates/data/2014 EIA 923/'
 eia923.file.s2t5 = 'EIA923_Schedules_2_3_4_5_M_12_2014_Final_Revision.xlsx' #923 schedules 2 thru 5
-eia860.file.s2 = '2___Plant_Y2014' #860 schedule 2
+eia860.file.s2 = '2___Plant_Y2014.xlsx' #860 schedule 2
 #output.dir    = '~/Documents/Graduate/Journal Papers/in-progress/EST_regional-water-rates'
 #setwd(wd)
 
@@ -40,7 +40,7 @@ page5.purchasedfuel = as.data.table(read.xlsx(eia923.file.s2t5, sheet = 5, start
 #EIA 860 spreadsheets
 #setwd('~')
 setwd(eia860.folder)
-plantinfo = as.data.table(read.xlsx(eia860.file.s2, sheet = 1, startRow = 3)) #information on location (lat, long)
+plantinfo = as.data.table(read.xlsx(eia860.file.s2, sheet = 1, startRow = 2)) #information on location (lat, long)
 
 
 # define constants --------------------------------------------------------
@@ -65,4 +65,19 @@ purchased.coal
 
 # primary energy water use ------------------------------------------------
 
-
+#calculate total fuel consumption (MMBTU) for each fuel category defined by EG
+plant.locations = plantinfo[,c(3,4,8,11,12)]
+fuel.categories = reported.fuels[,c(1)]
+##note to self: need to sum these values and deal with plants using "OTH", "PUR" and "WH" fuels
+fuel.categories$Hydropower = reported.fuels[,c("WAT")]
+fuel.categories$Solar = reported.fuels[,c("SUN")]
+fuel.categories$Wind = reported.fuels[,c("WND")]
+fuel.categories$Geothermal = reported.fuels[,c("GEO")]
+fuel.categories$Solid.Biomass.RDF = reported.fuels[,c("AB","BLQ","MSN","MSB","OBL","OBS","SLW","TDF","WDL","WDS")]
+fuel.categories$Biogas= reported.fuels[,c("LFG","OBG")]
+fuel.categories$Oil = reported.fuels[,c("DFO","JF","KER","PC","PG","RFO","SGP","WO")]
+fuel.categories$Natural.Gas = reported.fuels[,c("NG", "OG")]
+fuel.categories$Uranium = reported.fuels[,c("NUC")]
+fuel.categories$Lignite.Coal = reported.fuels[,c("LIG")]
+fuel.categories$Bituminous.Coal = reported.fuels[,c("BIT","BFG","RC","SC","SGC","WC")]
+fuel.categories$Subbituminous.Coal = reported.fuels[,c("SUB")]
